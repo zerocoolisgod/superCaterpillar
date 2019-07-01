390907fb35de338afdcca9a12797b921158b9e84
zerocoolisgod <zero.cool.is.god@gmail.com>
2019-07-01 13:41:10 -0700

    Map Work

--- maps/tilesheet_plains.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset name="tilesheet_plains" tilewidth="8" tileheight="8" spacing="2" margin="1" tilecount="60" columns="12">
- <image source="../graphics/tilesets/tilesheet_plains.png" width="120" height="50"/>
-</tileset>
